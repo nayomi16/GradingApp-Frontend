5e437c5dac2e0499481194f6cb63fdc926dcc725
nayomi16 <nayomirashmikaaa@gmail.com>
2021-02-24 02:28:07 +0530

    added bootstrap and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {TeacherComponent} from './views/teacher/teacher.component';
+import {LoginComponent} from './views/login/login.component';
+import {StudentComponent} from './views/student/student.component';
+
+const routes: Routes = [
+  {
+    path : 'student', component: StudentComponent
+  },
+  {
+    path : 'login', component : LoginComponent
+  },
+
+  {
+    path : 'teacher', component : TeacherComponent
+  }
+
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { StudentComponent } from './views/student/student.component';
+import { TeacherComponent } from './views/teacher/teacher.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StudentComponent,
+    TeacherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
