bf069dd0c741fa69c1caddf04b8842c467fb0f74
nayomi16 <nayomirashmikaaa@gmail.com>
2021-02-25 02:56:28 +0530

    added overall grade details

--- src/app/services/teacher.service.ts
+++ src/app/services/teacher.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {ResponseDto} from '../dto/response-dto';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
+  private URL = 'http://localhost:5000/teacher/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTeacher(teachId): Observable<ResponseDto> {
+    return this.http.get<ResponseDto>(this.URL + teachId);
+  }
+
+
+  getStatisticsOnQuestion(teachId: any, assId: any, qId: any): Observable<ResponseDto>  {
+    return this.http.get<ResponseDto>(this.URL + 'assignments/statics' + '/' + teachId + '/' + assId + '/' + qId);
+  }
+
+  getOverallGrade(teachId: any, assId: any): Observable<ResponseDto>  {
+    return this.http.get<ResponseDto>(this.URL + 'assignments/gradeDetail' + '/' + teachId + '/' + assId );
+
+  }
 }
--- src/app/views/teacher/teacher.component.ts
+++ src/app/views/teacher/teacher.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {TeacherService} from '../../services/teacher.service';
 
 @Component({
   selector: 'app-teacher',
@@ -6,11 +8,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher.component.css']
 })
 export class TeacherComponent implements OnInit {
+   teachId: any;
+   name: string;
+   class;
+   assId;
+   gradeDetail=[];
+   sId;
+   averageTime;
+   correctPercent;
+   questionView = false;
+   mainView = true;
+   staticView = false;
+   all=false;
+   teacherAssignDetails = [];
+   assgnDetail = [];
+   qIds = [];
+   qId: any;
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute, private teacherService: TeacherService) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((param) => {
+      this.teachId = param.userId;
+    });
+    this.teacherService.getTeacher(this.teachId).subscribe((response) => {
+
+      this.name = response.data.teacherObj.name;
+      this.class = response.data.teacherObj.class;
+      response.data.teacherAssgnDetails.forEach(assgnDetail => {
+          this.teacherAssignDetails.push(assgnDetail);
+
+
+      });
+
+    });
 
   }
+
+  showQuestions(qIds: any, assId: any) {
+    this.mainView = false;
+    this.questionView = true;
+    this.qIds = qIds;
+    this.assId = assId;
+    console.log(qIds);
+
+  }
+
+  showStatics(qId: any) {
+    this.mainView = false;
+    this.questionView = false;
+    this.staticView = true;
+
+    this.teacherService.getStatisticsOnQuestion(this.teachId, this.assId, qId).subscribe((response) => {
+      this.qId = response.data.qId;
+      this.averageTime = response.data.averageTimeSpent;
+      this.correctPercent = response.data.corectPercentage;
+      console.log(this.averageTime);
+    });
+  }
+  averallGrads(assId: any) {
+    this.mainView=false;
+    this.all=true;
+    this.teacherService.getOverallGrade(this.teachId,assId).subscribe((response)=>{
+        console.log(response.data)
+        this.gradeDetail=response.data;
+    })
+
+  }
+  
 
 }
